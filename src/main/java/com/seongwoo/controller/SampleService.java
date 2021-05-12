356d9040c57155f151557b5352440966a9db7532
swkim <kbkkim2@naver.com>
2021-05-13 01:07:21 +0900

    수정 : 롤백 후 테스트

--- src/main/java/com/seongwoo/controller/SampleService.java
+++ src/main/java/com/seongwoo/controller/SampleService.java
@@ -1,10 +1,11 @@
 package com.seongwoo.controller;
 
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class SampleService {
-    public String getName() {
-        return "test";
+    public static String getName() {
+        return "seongwoo";
     }
 }
